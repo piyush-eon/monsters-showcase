13c7dad6478dd18d631e6fee774ea4bc76d963b0
Piyush Agarwal <51760520+Piyush55dude@users.noreply.github.com>
2020-05-15 00:42:47 +0530

    Updates

---
+++ precache-manifest.3d3f303822097756c44057a4a5af37d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc2e945729bd18e9ebaf32eefad68ac",
+    "url": "/monsters-showcase/index.html"
+  },
+  {
+    "revision": "a2ec690bf1be5e31df0c",
+    "url": "/monsters-showcase/static/css/main.2767f1f0.chunk.css"
+  },
+  {
+    "revision": "a298b57edb8c592b96f5",
+    "url": "/monsters-showcase/static/js/2.97293c75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-showcase/static/js/2.97293c75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2ec690bf1be5e31df0c",
+    "url": "/monsters-showcase/static/js/main.430ae190.chunk.js"
+  },
+  {
+    "revision": "9a0e6f28aa97f2c3336c",
+    "url": "/monsters-showcase/static/js/runtime-main.94174144.js"
+  }
+]);
